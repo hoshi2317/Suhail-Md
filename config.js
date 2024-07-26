6503cf4990efe77e6fcb4c3e2c4c287cb68a2a03
Hoshi <91725235+hoshi2317@users.noreply.github.com>
2024-07-26 11:08:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ global.timezone= process.env.TZ || process.env.TIME_ZONE || "Asia/jakarta";
 global.github=process.env.GITHUB|| "https://github.com/hoshi2317/Suhail-Md";
 global.gurl  =process.env.GURL  || "https://whatsapp.com/channel/0029Va9thusJP20yWxQ6N643";
 global.website=process.env.GURL || "https://whatsapp.com/channel/0029Va9thusJP20yWxQ6N643" ; 
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://github.com/hoshi2317/Suhail-Md/lib/assets/suhail.jpg?raw=true" ; // SET LOGO FOR IMAGE 
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://github.com/hoshi2317/Suhail-Md/lib/assets/Hoshino.jpg" ; // SET LOGO FOR IMAGE 
 global.caption = process.env.CAPTION || global.caption || "ᴹᵃᵈᵉ ᵇʸ ʰᵒˢʰᶦ" 
 
 
